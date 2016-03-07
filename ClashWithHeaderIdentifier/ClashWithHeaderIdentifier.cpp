61127b17db639d4c63f2d5536eb099cf4c16aa47
mdk <mohammaddelkhah@gmail.com>
2016-03-07 16:59:47 +0330

    Committing the project for identifier clash. (It's actually for last week.)

---
+++ ClashWithHeaderIdentifier/ClashWithHeaderIdentifier.cpp
@@ -0,0 +1,19 @@
+// ClashWithHeaderIdentifier.cpp : Defines the entry point for the console application.
+//
+
+#include "stdafx.h"
+#include <iomanip>
+
+namespace my_namespace
+{
+	class time
+	{
+	};
+}
+
+int main()
+{
+	my_namespace::time a;
+    return 0;
+}
+
